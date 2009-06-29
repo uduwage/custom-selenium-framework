c1b6f2ad995eba659cc871fe917c8384f343c506
auduwage <auduwage@b4a15ece-64d3-11de-b126-f3c85089a223>
2009-06-29 20:48:30 +0000

    Input File package

---
+++ evalSelenium/src/com/cannon/selenium/input/InputFileFinder.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.cannon.selenium.input;
+
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author a83E1
+ *
+ */
+public class InputFileFinder {
+
+	private static final Logger log = Logger.getLogger(InputFileFinder.class.getName());
+	
+	private static final String DEFAULT_LANG_CODE = "en";
+	
+	public static InputStream getInputFileAsStream() {
+		return null;
+	}
+}
