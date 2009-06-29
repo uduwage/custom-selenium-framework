1e01d7262a3453d0a19a3fca9ce3a500d4161bc0
auduwage <auduwage@b4a15ece-64d3-11de-b126-f3c85089a223>
2009-06-29 18:56:02 +0000

    cooper package initial check-in

---
+++ trunk/evalSelenium/src/com/cooper/selenium/CooperTestCase.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.cooper.selenium;
+
+import java.io.InputStream;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * @author a83E1
+ *
+ */
+public class CooperTestCase {
+
+	private static final org.apache.log4j.Logger log = Logger.getLogger(CooperTestCase.class.getName());
+	
+	//Flag set to true when an input file is found and parse
+	private boolean inputFileFound = false;
+	
+	//used to hold all selenium session opened by the test so that we can close them 
+	//all down during exit.
+	private final ArrayList<AbstractSeleniumDriver> session = new ArrayList<AbstractSeleniumDriver>();
+	
+	private HashMap<String, Object> inputParams;
+	
+	/**
+	 * Default Constructor
+	 */
+	public CooperTestCase() {	
+	}
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	/**
+	 * Retrieves the value defined for a single-value parameter. if no matching 
+	 * parameter is found, or is no file exists, this method return null.
+	 * @param name Name of the single-value parameter to retrieve
+	 * @return The value defined for the specified parameter, or null if no 
+	 * matching parameter can be found.
+	 */
+	protected String getParameter(String name) {
+		//TODO: need to incoperate input file validation to this.
+		if(inputParams.get(name) == null) {
+			throw new InvalidParameterException("Input parameter \"" + name + "\" not found");
+		}
+		return (String)inputParams.get(name);
+	}
+	
+	protected String[] getParameters(String name) {
+		//TODO: need to incooperate input file validation with this.
+		if(inputParams.get(name) == null) {
+			throw new InvalidParameterException("Input parameter \"" + name + "\" not found");
+		}
+		String[] values = null;
+		try {
+			values = (String[])inputParams.get(name);
+		}catch (ClassCastException e) {
+			throw new InvalidParameterException("Input parameter \"" + name + "\" does not appear to be a " +
+    				"multi-value parameter.  Use the getParameter() method for single-value parameters" + e);
+		}
+		return values;
+	}
+	
+	@After
+	public void tearDown() {
+		//boolean autoClose = Boolean.parseBoolean()
+	}
+}
---
+++ trunk/evalSelenium/src/com/cooper/selenium/SeleniumSession.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.cooper.selenium;
+
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * @author a83E1
+ *
+ */
+public class SeleniumSession {
+
+	private static final org.apache.log4j.Logger log = Logger.getLogger(AbstractSeleniumDriver.class.getName());
+	
+	private static ThreadLocal<HashMap<String, AbstractSeleniumDriver>> cache = 
+		new ThreadLocal<HashMap<String,AbstractSeleniumDriver>>() {
+		protected synchronized HashMap<String, AbstractSeleniumDriver> intialValue() {
+			return new HashMap<String, AbstractSeleniumDriver>();
+		}
+	};
+	
+	private static ThreadLocal<AbstractSeleniumDriver> selenium = new ThreadLocal<AbstractSeleniumDriver>();
+	
+	/**
+	 * Retrieve the current Selenium Session
+	 * @return
+	 */
+	public static AbstractSeleniumDriver get() {
+		return selenium.get();
+	}
+	
+	/**
+	 * Set the current Selenium Session
+	 * @param instance
+	 */
+	public static void set(AbstractSeleniumDriver instance) {
+		selenium.set(instance);
+	}
+	
+	public static void end() {
+
+	}
+	
+}
