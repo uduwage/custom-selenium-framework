27e2418e915c83a1ce2391cde2f9ff7ee769af89
auduwage <auduwage@b4a15ece-64d3-11de-b126-f3c85089a223>
2009-06-29 21:46:23 +0000

    Input File package

--- evalSelenium/src/com/cannon/selenium/input/InputFileFinder.java
+++ evalSelenium/src/com/cannon/selenium/input/InputFileFinder.java
@@ -7,6 +7,8 @@ import java.io.InputStream;
 
 import org.apache.log4j.Logger;
 
+import com.cannon.selenium.CannonTestCase;
+
 /**
  * @author a83E1
  *
@@ -17,7 +19,35 @@ public class InputFileFinder {
 	
 	private static final String DEFAULT_LANG_CODE = "en";
 	
-	public static InputStream getInputFileAsStream() {
-		return null;
+	/**
+	 * 
+	 * @param test
+	 * @return
+	 */
+	public static InputStream getInputFileAsStream(CannonTestCase test) {
+		return test.getClass().getClassLoader().getResourceAsStream(getInputFileNameForTest(test));
+	}
+
+	/**
+	 * Determines the appropriate name and location of the input file for the given test
+	 * @param test
+	 * @return
+	 */
+	private static String getInputFileNameForTest(CannonTestCase test) {
+		//Check to see if we are in the default package; this happens often when we are running 
+		//from the Selenium Runner. if we are in the default package, null is returned 
+		//by the call to getPackage(). This must be handeld correctly.
+		String packageName = "";
+		if(test.getClass().getPackage() != null) {
+			packageName = test.getClass().getPackage().getName() + ".";
+		}
+		String resourceName = (packageName + test.getClass().getSimpleName()).replace('.', '/');
+		String languageCode = System.getProperty("user.lnaguage");
+		if(!languageCode.equals(DEFAULT_LANG_CODE)) {
+			resourceName += "_" + languageCode;
+		}
+		resourceName += ".xml";
+		log.info("Seaching for input file " + resourceName);
+		return resourceName;
 	}
 }
