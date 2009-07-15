4fd8bc38e52f698cf100b1dfa974a9d0048fde9f
auduwage <auduwage@b4a15ece-64d3-11de-b126-f3c85089a223>
2009-07-15 21:48:24 +0000

    More function to handle contact info page.

--- evalSelenium/src/com/cooper/selenium/ContactInfoPageSolvent.java
+++ evalSelenium/src/com/cooper/selenium/ContactInfoPageSolvent.java
@@ -10,17 +10,54 @@ import com.thoughtworks.selenium.SeleniumException;
  *
  */
 public class ContactInfoPageSolvent extends AbstractSeleniumDriver {
-	
-	public ContactInfoPageSolvent() {
-		//nothind to do at this point.
+
+	/**
+	 * @param text string value for name.
+	 */
+	public ContactInfoPageSolvent inputFirstName(String text) {
+		String locator = "//td[@class='contactLeft']//input[@name='firstName']";
+		waitForElement(locator);
+		if(!isElementPresent(locator))
+			throw new SeleniumException("Could not find input label firstName to type in");
+		selenium.type(locator, text);
+		return this;
 	}
 	
-	public ContactInfoPageSolvent inputFirstName(String text) {
-		selenium.setSpeed("5000");
-		String locator = "//table[@class='contactTable']//input[@name='firstName']";
-		if(!selenium.isElementPresent(locator))
-			throw new SeleniumException("Could not find input labele firstName to type in");
-		selenium.type(locator, text);
+	/**
+	 * Click update button on contactInfoPage
+	 */
+	public ContactInfoPageSolvent clickUpdateContact() {
+		String buttonLocator = "//table[@class='contactTable']//input[@type='submit' and @name='update']";
+		if(!selenium.isElementPresent(buttonLocator))
+			throw new SeleniumException("Could not find update button to click");
+		selenium.click(buttonLocator);
+		return this;
+	}
+	
+	/**
+	 * select the phone type on contact page.
+	 * @param phoneType string value of the phone type.
+	 * @return
+	 */
+	public ContactInfoPageSolvent selectPhoneNumberType(String phoneType) {
+		String phoneTypeLocator = "//select[@name='notificationType_1']";
+		if(!selenium.isElementPresent(phoneTypeLocator))
+			throw new SeleniumException("Could not find Phone Type Drop Down Menu");
+		selenium.select(phoneTypeLocator, phoneType);
+		return this;
+	}
+	
+	/**
+	 * Click any button with a given button name, on the contact page.
+	 * @param buttonName Any input type which are buttons
+	 * @return
+	 */
+	public ContactInfoPageSolvent clickInfoPageButton(String buttonName) {
+		String buttonLocator = "//input[@type='submit' and @value='" + buttonName + "']";
+		waitForElement(buttonLocator);
+		if(!isElementPresent(buttonLocator))
+			throw new SeleniumException("Could not find button " + buttonName + " to click");
+		selenium.click(buttonLocator);
 		return this;
 	}
 }
