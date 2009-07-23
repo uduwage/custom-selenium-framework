7d34e38d5b814ab9087fbc8560fc681d916b8720
auduwage <auduwage@b4a15ece-64d3-11de-b126-f3c85089a223>
2009-07-23 16:00:06 +0000

    Refactor into more sensible package structure.

---
+++ trunk/evalSelenium/src/com/cooper/selenium/stars/ContactInfoPageSolvent.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.cooper.selenium.stars;
+
+import com.cooper.selenium.AbstractSeleniumDriver;
+import com.thoughtworks.selenium.SeleniumException;
+
+/**
+ * @author anuradha.uduwage
+ *
+ */
+public class ContactInfoPageSolvent extends AbstractSeleniumDriver {
+
+	/**
+	 * @param text string value for name.
+	 */
+	public ContactInfoPageSolvent inputFirstName(String text) {
+		String locator = "//td[@class='contactLeft']//input[@name='firstName']";
+		waitForElement(locator);
+		if(!isElementPresent(locator))
+			throw new SeleniumException("Could not find input label firstName to type in");
+		selenium.type(locator, text);
+		return this;
+	}
+	
+	/**
+	 * Click update button on contactInfoPage
+	 */
+	public ContactInfoPageSolvent clickUpdateContact() {
+		String buttonLocator = "//table[@class='contactTable']//input[@type='submit' and @name='update']";
+		if(!selenium.isElementPresent(buttonLocator))
+			throw new SeleniumException("Could not find update button to click");
+		selenium.click(buttonLocator);
+		return this;
+	}
+	
+	/**
+	 * select the phone type on contact page.
+	 * @param phoneType string value of the phone type.
+	 * @return
+	 */
+	public ContactInfoPageSolvent selectPhoneNumberType(String phoneType) {
+		String phoneTypeLocator = "//select[@name='notificationType_1']";
+		if(!selenium.isElementPresent(phoneTypeLocator))
+			throw new SeleniumException("Could not find Phone Type Drop Down Menu");
+		selenium.select(phoneTypeLocator, phoneType);
+		return this;
+	}
+	
+	/**
+	 * Click any button with a given button name, on the contact page.
+	 * @param buttonName Any input type which are buttons
+	 * @return
+	 */
+	public ContactInfoPageSolvent clickInfoPageButton(String buttonName) {
+		String buttonLocator = "//input[@type='submit' and @value='" + buttonName + "']";
+		waitForElement(buttonLocator);
+		if(!isElementPresent(buttonLocator))
+			throw new SeleniumException("Could not find button " + buttonName + " to click");
+		selenium.click(buttonLocator);
+		return this;
+	}
+}
