919cb3314767e57aba8a5857169f7f2ebbe9f063
auduwage <auduwage@b4a15ece-64d3-11de-b126-f3c85089a223>
2009-07-24 20:34:00 +0000

    class to handle data file finder. Increase automation.

---
+++ evalSelenium/src/com/cooper/selenium/input/DataFileFinder.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.cooper.selenium.input;
+
+import java.io.InputStream;
+
+import org.apache.log4j.Logger;
+
+import com.cooper.selenium.SeleniumSession;
+import com.cooper.selenium.SolventSeleniumTestCase;
+
+/**
+ * This class fille find if there is an input file for the test case.
+ * @author anuradha.uduwage
+ *
+ */
+public class DataFileFinder {
+
+	private static final Logger log = Logger.getLogger(DataFileFinder.class.getName());
+	
+	/**
+	 * 
+	 * @param testcase
+	 * @return
+	 */
+	public static InputStream getDataInputFileAsStream(SolventSeleniumTestCase testcase) {
+		return testcase.getClass().getClassLoader().getResourceAsStream(getDataFileNameForTest(testcase));
+	}
+
+	/**
+	 * Determines the appropriate name and the location of the input file for the given testcase.
+	 * @param testcase 
+	 * @return return the file name
+	 */
+	private static String getDataFileNameForTest(SolventSeleniumTestCase testcase) {
+		String packageName = "";
+		if(testcase.getClass().getPackage() != null) {
+			packageName = testcase.getClass().getPackage().getName() + ".";
+		}
+		String resourceName = (packageName + testcase.getClass().getSimpleName()).replace('.', '/');
+		resourceName += ".xml";
+		return resourceName;
+	}
+}
