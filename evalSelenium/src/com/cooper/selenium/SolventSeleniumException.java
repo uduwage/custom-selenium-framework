3e950dfea78f1ebdee2e918ed33b13cb230a423d
auduwage <auduwage@b4a15ece-64d3-11de-b126-f3c85089a223>
2009-08-19 21:42:28 +0000

    Adding exception handling custom to our framework

---
+++ evalSelenium/src/com/cooper/selenium/SolventSeleniumException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.cooper.selenium;
+
+/**
+ * @author A83E1
+ *
+ */
+public class SolventSeleniumException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public SolventSeleniumException(String stringMsg) {
+		super(stringMsg);
+	}
+}
