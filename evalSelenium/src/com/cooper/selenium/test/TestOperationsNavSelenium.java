ddd1353a338cbb61bc1abea2347eeed255bc2907
auduwage <auduwage@b4a15ece-64d3-11de-b126-f3c85089a223>
2009-08-24 20:10:47 +0000

    Selenium test to navigate functions in operations page.

---
+++ evalSelenium/src/com/cooper/selenium/test/TestOperationsNavSelenium.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.cooper.selenium.test;
+
+import org.junit.Test;
+
+import com.cooper.selenium.SolventSeleniumTestCase;
+import com.cooper.selenium.common.LoginLogoutSolvent;
+import com.cooper.selenium.common.OperationsPageSolvent;
+import com.cooper.selenium.common.YukonTopMenuSolvent;
+
+/**
+ * Test to check the navigation options in /Operations.jsp page
+ * @author anuradha.uduwage
+ *
+ */
+
+public class TestOperationsNavSelenium extends SolventSeleniumTestCase {
+
+	@Test
+	public void testNavigate() {
+		//start the session for the test. 
+		start();
+		//use the LoginSolvent to login
+		LoginLogoutSolvent loginLogoutSolvent = new LoginLogoutSolvent();
+		loginLogoutSolvent.cannonLogin("yukon", "yukon");
+		//Click multiple functions in operations page.
+		OperationsPageSolvent operationsPageSolvent = new OperationsPageSolvent();
+		operationsPageSolvent.clickLinkItem("All Trends").navigateTo(new YukonTopMenuSolvent()).clickAllTrendsHome();
+		operationsPageSolvent.clickLinkItem("Bulk Importer");
+		operationsPageSolvent.navigateTo(new YukonTopMenuSolvent()).clickHome();
+		operationsPageSolvent.clickLinkItem("Metering").navigateTo(new YukonTopMenuSolvent().clickHome());
+		
+		//showing the versatility
+		YukonTopMenuSolvent menuSolvent = new YukonTopMenuSolvent();
+		operationsPageSolvent.clickLinkItem("Bulk Operations");
+		menuSolvent.clickHome();
+		operationsPageSolvent.clickLinkItem("Direct");
+		menuSolvent.clickAllTrendsHome();
+		operationsPageSolvent.clickLinkItem("3-Tier Direct");
+		menuSolvent.clickAllTrendsHome();
+		operationsPageSolvent.clickLinkItem("Cap Control");
+		menuSolvent.clickHome();
+		operationsPageSolvent.clickLinkItem("Reporting");
+		menuSolvent.clickHome();
+		operationsPageSolvent.clickLinkItem("Commander");
+		menuSolvent.clickHome();
+		operationsPageSolvent.clickLinkItem("Manage Indexes");
+		menuSolvent.clickHome();
+		operationsPageSolvent.clickLinkItem("Device Configuration");
+		//logout
+		loginLogoutSolvent.yukonLogout();
+		//kill everything so no leftover hanging sessions.
+		tearDown();
+		
+	}
+
+}
