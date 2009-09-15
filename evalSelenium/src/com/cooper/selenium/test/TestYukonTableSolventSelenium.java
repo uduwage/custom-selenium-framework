4b27bb762b320af7cafa39620c229ee5dd4bbbd6
auduwage <auduwage@b4a15ece-64d3-11de-b126-f3c85089a223>
2009-09-15 19:26:43 +0000

    Test Yukon Table solvent

---
+++ evalSelenium/src/com/cooper/selenium/test/TestYukonTableSolventSelenium.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.cooper.selenium.test;
+
+
+import org.junit.Test;
+
+import com.cooper.selenium.SolventSeleniumTestCase;
+import com.cooper.selenium.common.LoginLogoutSolvent;
+import com.cooper.selenium.common.OperationsPageSolvent;
+import com.cooper.selenium.common.YukonTableSolvent;
+import com.cooper.selenium.meetering.MeteringSolvent;
+
+/**
+ * @author anuradha.uduwage
+ *
+ */
+
+public class TestYukonTableSolventSelenium extends SolventSeleniumTestCase {
+	
+	@Test
+	public void testTable() {
+		this.start(this.getAuthenticatedSeleniumSession(), new LoginLogoutSolvent())
+			.cannonLogin("yukon", "yukon")
+			.navigateTo(new OperationsPageSolvent()).clickLinkItem("Metering")
+			.navigateTo(new MeteringSolvent()).clickMeterSearch()
+			.navigateTo(new YukonTableSolvent("tableId=deviceTable"))
+			.getTextInCell(2, 1);
+			
+		
+	}
+
+}
