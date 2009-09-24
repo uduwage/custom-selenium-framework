4942292ccc46db8b72a8f9dc156f24510d4f7798
auduwage <auduwage@b4a15ece-64d3-11de-b126-f3c85089a223>
2009-09-24 20:49:56 +0000

    incremental updates.

--- evalSelenium/src/com/cooper/selenium/test/TestAuthenticationSelenium.java
+++ evalSelenium/src/com/cooper/selenium/test/TestAuthenticationSelenium.java
@@ -11,6 +11,7 @@ import junit.textui.TestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.cooper.selenium.SeleniumDefaultProperties;
 import com.cooper.selenium.SolventSeleniumTestCase;
 import com.cooper.selenium.common.CommonSolvent;
 import com.cooper.selenium.common.LoginLogoutSolvent;
@@ -39,19 +40,27 @@ public class TestAuthenticationSelenium extends SolventSeleniumTestCase {
 			.navigateTo(new YukonTopMenuSolvent()).clickHome();
 		
 		YukonTopMenuSolvent menuSolvent = new YukonTopMenuSolvent();
-		menuSolvent.selectALocation("Bulk Importer").clickBreadcrumb("Operations Home");
+		menuSolvent.selectALocation("Bulk Importer");
+		
+		CommonSolvent commonSolvent = new CommonSolvent();
+		Assert.assertEquals("Bulk Importer", commonSolvent.getPageTitle());
+		Assert.assertEquals("Bulk Importer", commonSolvent.getYukonText("Bulk Importer"));
+		
+		menuSolvent.clickBreadcrumb("Operations Home");
 		menuSolvent.clickHome();
 		menuSolvent.selectALocation("Reporting");
 		menuSolvent.clickHome();
-		menuSolvent.selectALocation("Metering");
-		menuSolvent.clickHome();
-		menuSolvent.selectALocation("Bulk Operations");
 		
+		menuSolvent.selectALocation("Metering");
+		Assert.assertEquals("Meter Home Page", commonSolvent.getPageTitle());
+		menuSolvent.clickHome();
+		
+		menuSolvent.selectALocation("Bulk Operations");
 		//test if we are in Bulk Operations page
-		CommonSolvent commonSolvent = new CommonSolvent();
+		Assert.assertEquals("Bulk Operations", commonSolvent.getPageTitle());
 		Assert.assertEquals("Bulk Operations", commonSolvent.getYukonText("Bulk Operations"));
 		
-		operationPage.yukonLogout();
+		operationPage.yukonLogout().end();
 	}
 	
 	@Test
@@ -67,12 +76,11 @@ public class TestAuthenticationSelenium extends SolventSeleniumTestCase {
 				if(users[i] != null) {
 					loginLogoutSolvent.cannonLogin(users[i], passwords[i]);
 					generalSolvent.clickGeneral();
-					generalSolvent.clickStarsLeftMenuLink("Contacts");
+					generalSolvent.clickStarsLeftMenuLink("Contact Us");
 					generalSolvent.clickStarsLeftMenuLink(users[i]);
 					generalSolvent.clickStarsLeftMenuLink("Schedule");
 					generalSolvent.clickStarsLeftMenuLink("Control History");
-					generalSolvent.clickStarsLeftMenuLink("Enrollment");
-					generalSolvent.clickStarsLeftMenuLink("Opt Out");
+					//generalSolvent.clickStarsLeftMenuLink("Opt Out");
 					loginLogoutSolvent.yukonLogout();
 				}
 				else 
@@ -80,6 +88,7 @@ public class TestAuthenticationSelenium extends SolventSeleniumTestCase {
 				
 			}
 		}
+		loginLogoutSolvent.end();
 	}
 	
 	@Test
@@ -88,6 +97,7 @@ public class TestAuthenticationSelenium extends SolventSeleniumTestCase {
 		LoginLogoutSolvent loginLogoutSolvent = new LoginLogoutSolvent();
 		String[] users = getParamStrings("users");
 		String[] passwords = getParamStrings("passwords");
+		String http500 = "Apache Tomcat/5.5.27 - Error report";
 		CommonSolvent commonSolvent = new CommonSolvent();
 		for(int i=0; i < users.length; i++) {
 			if((passwords.length != 0) && (passwords.length == users.length)) {
@@ -95,6 +105,7 @@ public class TestAuthenticationSelenium extends SolventSeleniumTestCase {
 					loginLogoutSolvent.cannonLogin(users[i], passwords[i]);
 					commonSolvent.openURL("http://pspl-qa008:8080/setup.jsp");
 					commonSolvent.getYukonText("HTTP Status");
+					Assert.assertEquals(http500, commonSolvent.getPageTitle());
 					commonSolvent.openURL("http://pspl-qa008:8080/");
 					loginLogoutSolvent.yukonLogout();
 				}
@@ -102,19 +113,31 @@ public class TestAuthenticationSelenium extends SolventSeleniumTestCase {
 					throw new SeleniumException("Users Didn't get Extracted properly from XML");
 				
 			}
-		}		
+		}
+		
+		loginLogoutSolvent.cannonLogin(getParamString("username"), getParamString("password"));
+		commonSolvent.openURL("http://pspl-qa008:8080/setup.jsp");
+		Assert.assertFalse(commonSolvent.isPageTitle(http500));		
+		commonSolvent.end();
+		
 	}
 	
 	@Test
 	public void testNegativeLogin() {
 		init();
 		LoginLogoutSolvent loginLogoutSolvent = new LoginLogoutSolvent();
-		loginLogoutSolvent.cannonLogin(getParamString("invaliduser"), getParamString("password")).end();
+		loginLogoutSolvent.cannonLogin(getParamString("invaliduser"), getParamString("password"));
+		loginLogoutSolvent.navigateTo(new CommonSolvent()).getYukonText("Invalid username");
+		loginLogoutSolvent.end();
 	}
 	@Test
-	public void testOtherTests() {
+	public void testOperationsNav() {
 		TestOperationsNavSelenium operSel = new TestOperationsNavSelenium();
 		operSel.testNavigate();
 	}
-	
+	@Test
+	public void testMeterDeviceTable() {
+		TestYukonTableSolventSelenium table = new TestYukonTableSolventSelenium();
+		table.testTable();
+	}
 }
